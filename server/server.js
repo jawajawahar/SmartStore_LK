70c4c5de4ca6e094e0d194ddf1e9c0d91fd4b49a
jawajawahar <jawajawaharsha@gmail.com>
2026-06-04 07:39:59 +0530

    Render deployment fix

--- server/server.js
+++ server/server.js
@@ -44,8 +44,16 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+
+    const PORT =
+      process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server running on port ${PORT}`
+      );
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) =>
+    console.log(err)
+  );
